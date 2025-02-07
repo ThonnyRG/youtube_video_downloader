da560fa920a3233fed307f57280d6141f353df6d
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-02-07 02:14:03 -0600

    Making the GUI alert

---
+++ UI/AlertFrame.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class AlertFrame():
+
+    
+    def __init__(Self, AlertName):
+        Self.AlertName = AlertName
+        
+
+        
+    def setAlert(type):
+        Alert = tk.Tk()
+        Alert.geometry("200x200")
+        Alert.title("Warning")
+        Label = tk.Label(Alert, text = type.AlertName, font = ('Google', 16))
+       
--- UI/frameWithe.py
+++ UI/frameWithe.py
@@ -2,9 +2,9 @@ import tkinter as tk
 
 class FrameWithe():
    root = tk.Tk()
-   
+  
    root.geometry("500x500")
-   root.title("Tiitle gui")
+   root.title("Title gui")
    
    root.mainloop()
    
