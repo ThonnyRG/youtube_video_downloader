40de1a1596ce8ac5947f32e783547b6cedab2d1e
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-02-23 12:30:17 -0600

    Redisign

--- UI/frameBlank.py
+++ UI/frameBlank.py
@@ -19,17 +19,19 @@ class FrameWhite():
    labelSave = tk.Label(root, text = "Save to:", font="Roboto 20", bg= "#222222") 
    labelSave.place(x = 210, y = 140)
 
+   downloadButton = ctk.CTkEntry(root,placeholder_text = "Users/macbook/Downloads" ,height= 40, width=320)
+   downloadButton.place( x = 50, y = 175)
 
-   downloadButton = tk.Label(root, text = "C:Users/downloads",font = "Roboto 20" , fg = "#4285F4", bd = 0, highlightbackground = '#222222')
-   downloadButton.place( x = 170, y = 175)
-
-   entrylabel = ctk.CTkEntry(root, placeholder_text="ENTER URL VIDEO", font="Roboto 24", height= 40, width=400)
+   entrylabel = ctk.CTkEntry(root, placeholder_text="ENTER URL VIDEO",height= 40, width=400)
    entrylabel.place( x = 50, y = 237)
+  
+   browseButton = mtk.Button(root, text = "Browse", font = "Roboto 20", fg = "#174EA6", bd = 0, bg = "#222222")
+   browseButton.place(x = 355, y = 175)
    
-   clearButton = mtk.Button(root, text = "Clear",font = "Roboto 20" , fg = "#174EA6", bd = 0, bg = "#174EA6")
+   clearButton = mtk.Button(root, text = "Clear",font = "Roboto 20" , fg = "#174EA6", bd = 0, bg = "#222222")
    clearButton.place(x = 50, y = 300) 
   
-   pasteButton = mtk.Button(root, text = "Paste",font = "Roboto 20" , fg = "#174EA6", bd = 0, bg = "#174EA6")
+   pasteButton = mtk.Button(root, text = "Paste",font = "Roboto 20" , fg = "#174EA6", bd = 0, bg = "#222222")
    pasteButton.place(x = 200, y = 300) 
   
    downloadButton = mtk.Button(root, text = "Download",font = "Roboto 20" , fg = "#4285F4", bd = 0, bg = "#4E4E4E")
