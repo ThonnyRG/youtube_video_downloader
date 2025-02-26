f9025ac751c61430936582d3b2c7fa46d2e93767
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-02-25 19:27:09 -0600

    modified in alert frame

--- UI/AlertFrame.py
+++ UI/AlertFrame.py
@@ -6,11 +6,13 @@ class AlertFrame():
     def __init__(Self, AlertName):
         Self.AlertName = AlertName
         
-
-        
     def setAlert(type):
         Alert = tk.Tk()
-        Alert.geometry("200x200")
+        Alert.geometry("500x200")
         Alert.title("Warning")
+        Alert.resizable(width = False, height= False)
         Label = tk.Label(Alert, text = type.AlertName, font = ('Roboto', 16))
+        Label.place(x=10,y=20)
+        
+        Alert.mainloop()
        
