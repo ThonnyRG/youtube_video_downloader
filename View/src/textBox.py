77087450b6c53d8e20db13882e138d3db98458cb
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-07-15 15:25:50 -0600

    Features and fixes in Label Dactory

---
+++ View/src/textBox.py
@@ -0,0 +1,28 @@
+import customtkinter as ctk
+
+class textBox():
+    def __init__(self, frame):
+        self._textBox = ctk.CTkEntry(frame)
+        
+    def setText(self, message: str):
+        self._textBox.configure(placeholder_text = message)
+        return self
+        
+    def setFont(self, font : str, size: int):
+        self._textBox.configure(font =(font, size))
+        return self
+    
+    def setColor(self, color: str):
+        self._textBox.configure(text_color = color)
+        return self
+    
+    def setSize(self, h : int, w : int):
+        self._textBox.configure(height = h, width = w)
+        return self
+        
+    def getText(self):
+        return self._textBox.get()
+        
+    def setPosition(self, x: int, y : int):
+        self._textBox.place(x = x, y = y)
+        
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from View.frameBlank import FrameWhite
 from View.src.label import label
+from View.src.textBox import textBox
 
 def main():
     blankP = FrameWhite()
     labelTittle = label(blankP.getRoot())
     labelTittle.setFont("Roboto 24 bold")
-    labelTittle.setText("PYTHON IS TRASH")
+    labelTittle.setText("Youtube Video downloader")
     labelTittle.setBackground("#222222")
     labelTittle.setPosition(100, 40)
+    linkField = textBox(blankP.getRoot())
+    linkField.setText("Python is trash")
+    linkField.setFont("roboto", 22)
+    linkField.setSize(45,450)
+    linkField.setPosition(30,237)
+    linkField.setColor("#9AA0A6")
+    
     blankP.show()
     
 if __name__=="__main__":
