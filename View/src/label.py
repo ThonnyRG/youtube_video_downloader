2dbbfaf89afbeccb07abf302d5bb201bcbfb456b
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-07-15 09:37:55 -0600

    Created label factory

---
+++ View/src/label.py
@@ -0,0 +1,22 @@
+import tkinter as tk 
+
+class label():
+    def __init__(self, frame):
+        self._label = tk.Label(frame)
+        
+    def setText(self, message: str):
+        self._label.config(text = message)
+        return self
+    
+    def setFont(self, customFont: str):
+        self._label.config(font = customFont)
+        return self
+        
+    def setPosition(self, xPlace: int, yPlace: int):
+        self._label.place(x = xPlace, y = yPlace)
+        
+    def setBackground(self, background : str):
+        self._label.config(bg = background)
+    
+    def getLabel(self):
+        return self._label
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from View.frameBlank import FrameWhite
-
+from View.src.label import label
 
 def main():
     blankP = FrameWhite()
+    labelTittle = label(blankP.getRoot())
+    labelTittle.setFont("Roboto 24 bold")
+    labelTittle.setText("PYTHON IS TRASH")
+    labelTittle.setBackground("#222222")
+    labelTittle.setPosition(100, 40)
     blankP.show()
+    
 if __name__=="__main__":
     main()
